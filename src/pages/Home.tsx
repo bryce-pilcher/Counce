144ecd083e7a3653577e5468c6dda9fd63f3738a
Bryce Pilcher <bryce.pilcher@gmail.com>
2019-11-02 19:19:25 -0400

    intial app

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,26 +1,75 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import React from 'react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonButton, IonItem} from '@ionic/react';
+import React, { Component } from 'react';
+import { firestore } from '../firebase/FirebaseConf';
 
-const Home: React.FC = () => {
-  return (
-    <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Ionic Blank</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent className="ion-padding">
-        The world is your oyster.
-        <p>
-          If you get lost, the{' '}
-          <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/">
-            docs
-          </a>{' '}
-          will be your guide.
-        </p>
-      </IonContent>
-    </IonPage>
-  );
+export interface HomeProps {id: string}
+
+interface HomeState {count: number, listener: () => void}
+
+class Home extends React.Component<HomeProps, HomeState> {
+
+  constructor(props: HomeProps) {
+    super(props);
+
+    this.state = {
+      count: 0,
+      listener: () => {}
+    }
+  }
+
+  attachListener = () => {
+    let self = this;
+    return firestore.collection("count").doc("1").onSnapshot(function(snapshot) {
+        let c = snapshot.get("count")
+        self.setState({
+          count: c
+        })
+    })
+  }
+
+  componentDidMount() {
+    this.setState({
+      listener: this.attachListener()
+    })
+  }
+
+  handleIncrement = () => {
+    let self = this;
+    let c = self.state.count;
+    c += 1;
+    firestore.collection("count").doc("1").set({count: c})
+    firestore.collection("count").doc("1").collection("arrivals").add({time: Date.now()})
+  }
+
+  handleDecrement = () => {
+    let self = this;
+    let c = self.state.count;
+    c -= 1;
+    firestore.collection("count").doc("1").set({count: c})
+    firestore.collection("count").doc("1").collection("arrivals").orderBy("time", "desc").limit(1).get().then(function(d) {
+      let id = d.docs[0].id
+      firestore.collection("count").doc("1").collection("arrivals").doc(id).delete()
+    })
+  }
+
+  render() {
+
+    return (
+      <IonPage>
+        <IonHeader>
+          <IonToolbar>
+            <IonTitle>Counce</IonTitle>
+          </IonToolbar>
+        </IonHeader>
+        <IonContent className="ion-padding">
+          <div style={{textAlign:"center", fontSize:36+"px"}}>{this.state.count}</div>
+          <IonButton expand="full" color="success" onClick={this.handleIncrement}>One More</IonButton>
+          <div style={{height: 100+"px"}}></div>
+          <IonButton expand="full" color="warning" onClick={this.handleDecrement}>One Less</IonButton>
+        </IonContent>
+      </IonPage>
+    );
+  }
 };
 
 export default Home;
